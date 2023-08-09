cc3925784f8bdbba666f1d476faf6d6f06da789a
Prateek Maru <prateekmaru@yahoo.com>
2023-08-09 10:33:53 +0530

    update list

--- debloater_template.cmd
+++ debloater_template.cmd
@@ -9,7 +9,30 @@ adb devices
 ECHO PRESS ANY KEY TO START DE-BLOATING...........
 pause
 for %%X in (
-"com.android.chrome"
+    "com.android.stk"
+    "com.google.android.feedback"
+    "com.google.android.youtube"
+    "com.miui.analytics"
+    "com.miui.bugreport"
+    "com.miui.msa.global"
+    "com.miui.yellowpage"
+    "com.xiaomi.joyose"
+    "com.facebook.system"
+    "com.facebook.appmanager"
+    "com.facebook.services"
+    "com.xiaomi.payment"
+    "com.miui.miservice"
+    "com.tencent.soter.soterserver"
+    "com.miui.daemon"
+    "com.miui.cloudbackup"
+    "com.miui.mi.healthglobal"
+    "com.android.wallpaper.livepicker"
+    "com.miui.micloudsync"
+    "com.opera.browser.afin"
+    "com.google.android.apps.subscriptions.red"
+    "com.google.android.apps.nbu.paisa.user"
+    "com.xiaomi.glgm"
+    "com.miui.cloudservice"
  ) do (
     adb shell pm uninstall %%X
     adb shell pm uninstall --user 0 %%X
