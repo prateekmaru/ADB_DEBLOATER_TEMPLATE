81487e6273e8ee0983e1f2b5745bb47cff0d59dc
Prateek Maru <prateek@prateekspace.eu.org>
2023-09-30 17:51:27 +0530

    update 14.0.4 bloatware list from sky

--- los.cmd
+++ los.cmd
@@ -6,18 +6,20 @@ cd %Y%
 adb kill-server
 adb start-server
 adb devices
+adb root
 ECHO PRESS ANY KEY TO START DE-BLOATING...........
 pause
 for %%X in (
     "com.android.stk"
     "org.lineageos.audiofx"
     "com.android.calculator2"
-    "org.lineageos.aperture"
     "org.lineageos.jelly"
     "org.lineageos.etar"
-    "com.android.contacts"
     "com.android.gallery3d"
     "org.lineageos.eleven"
+    "org.omnirom.logcat"
+    "org.chromium.chrome"
+    "com.android.chrome"
  ) do (
     adb shell pm uninstall %%X
     adb shell pm uninstall --user 0 %%X
